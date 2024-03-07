e55079df897afdaac0f731acc135421455c74a18
sohansatoru <160564688+sohansatoru@users.noreply.github.com>
2024-03-07 11:14:47 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,23 @@
+    
+function preload(){
+    img=loadImage("bottle.jpg");
+    }
+    
+    function setup(){
+        canvas = createCanvas(640,420);
+        canvas.center();
+       
+    
+    }
+    
+    function draw(){
+        image( img ,0 ,0 ,640 ,420);
+        fill("#FF0000");
+        text("bed", 90 ,240);
+        noFill();
+        stroke('#FF0000')
+        rect(90, 225, 450, 500);
+    
+    
+    }
+    
